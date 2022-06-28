6aa99e1231bc30a80a6fe5bf7ecc7265c3476316
gen4ukk <gen4ukk@gmail.com>
2022-06-28 21:57:47 +0300

    strategy

---
+++ Strategy/IStrategy.cs
@@ -0,0 +1,38 @@
+﻿namespace Strategy
+{
+    internal interface IStrategy
+    {
+        void Display(string text);
+    }
+
+    internal class ConsoleStrategy : IStrategy
+    {
+        public void Display(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+    internal class FileStrategy : IStrategy
+    {
+        public void Display(string text)
+        {
+            File.WriteAllText("D:\\1.txt", text);
+        }
+    }
+
+    internal class TextSaverManager 
+    {
+        private IStrategy _strategy;
+
+        public void SetStrategy(IStrategy strategy) 
+        {
+            _strategy = strategy;
+        }
+
+        public void Display(string text)
+        {
+            _strategy.Display(text);
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Strategy;
+
+Console.WriteLine("Hello, World!");
+
+TextSaverManager manager = new TextSaverManager();
+
+manager.SetStrategy(new ConsoleStrategy());
+
+manager.Display("strategy 1");
+
+manager.SetStrategy(new FileStrategy());
+
+manager.Display("strategy 2");
